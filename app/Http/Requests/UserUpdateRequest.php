3d3a61f24de4de8f6e2ef7a19647f44f92da7695
Juan Donquis <juandonquis07@gmail.com>
2024-10-01 11:46:26 -0400

    ni

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -23,7 +23,7 @@ class UserUpdateRequest extends FormRequest
      */
     public function rules(): array
     {   
-        $userId = $this->route('user'); 
+        $userId = $this->route('usuario'); 
 
         return [
             
